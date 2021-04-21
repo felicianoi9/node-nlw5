80e48151c866b4d4eb14ff108f488a2afa5d5f90
Rogério Feliciano dos Santos <felicianoi9@gmail.com>
2021-04-21 13:29:39 -0300

    Created repository and routes config

--- src/server.ts
+++ src/server.ts
@@ -2,23 +2,12 @@ import express from "express"
 
 import "./database"
 
+import { routes } from "./routes"
+
 const app = express()
 
-// app.get("/", (request, response) => {
-//     return response.send("Olá NLW5")
-// })
-app.get("/", (request, response) => {
-    return response.json({
-        message: "Olá NLW 05",
-        author: "Rogério Feliciano"
-    })
-})
+app.use(express.json())
 
-app.post("/users", (request, response) => {
-    return response.json({
-        message: "Usuário cadastrado com  sucesso!!"
-    })
-})
-
+app.use(routes)
 
 app.listen(3333, () => console.log('Server is runing on 3333 port...'));
